1344cdfd4635c262523ed2b2368f156b41299250
LokeshSinh <lokeshsingh.jk@gmail.com>
2026-05-18 12:14:50 +0530

    first commit

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import logo from '../../../assets/Footer/logo.png'
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaLinkedinIn,
+    FaTwitter
+} from "react-icons/fa";
 
 function Footer() {
     return (
@@ -10,7 +17,7 @@ function Footer() {
                 <div className={styles.FooterName}>
                     <div className={styles.FooterLine}></div>
                     <h1>IMPROVE YOUR FUTURE</h1>
-                     <div className={styles.FooterLines}></div>
+                    <div className={styles.FooterLines}></div>
                 </div>
 
                 {/* footer */}
@@ -19,12 +26,60 @@ function Footer() {
                     {/* TextsPara */}
                     <div className={styles.FooterClass}>
                         <div className={styles.FooterImgText}>
-                            <img />
+                            <img src={logo} alt='logo' />
+                            <h3>ThinkClear</h3>
+                        </div>
+                        <p>ThinkClear helps students from Classes 6–12 build strong concepts
+                            in Maths and Science through clear explanations,
+                            structured learning, regular tests, and personalized guidance.</p>
+                        <div className={styles.SocialIcons}>
+                            <FaFacebookF />
+                            <FaInstagram />
+                            <FaLinkedinIn />
+                            <FaTwitter />
                         </div>
                     </div>
                     {/* Quiks LInnks */}
+
+                    <div className={styles.FooterQuciK}>
+                        <h4>Quick Links</h4>
+                        <div className={styles.FooterLinks}>
+                            <span>Home</span>
+                            <span>Courses</span>
+                            <span>About Us</span>
+                            <span>Contact</span>
+                            <span>Policies</span>
+                        </div>
+
+                    </div>
+                    {/* Quick */}
+                           <div className={styles.FooterQuciK}>
+                        <h4>Privacy Policy</h4>
+                        <div className={styles.FooterLinks}>
+                            <span>Information collected from users</span>
+                            <span>Purpose of data usage</span>
+                            <span>Refund eligibility</span>
+                            <span>Time period for refund</span>
+                            <span>Conditions or limitations</span>
+                        </div>
+
+                    </div>
+                    {/* Quick */}
+                           <div className={styles.FooterQuciK}>
+                        <h4>Contact Information</h4>
+                        <div className={styles.FooterLinks}>
+                            <span>+447575441703</span>
+                            <span>info@thinkclear.in</span>
+                            <span>WhatsApp Support</span>
+                        </div>
+
+                    </div>
                 </div>
-                {/*  */}
+                {/* Links */}
+                <div className={styles.LastLinks}>
+                    <p>© 2026 ThinkClear. All Rights Reserved.</p>
+                    <p>© Designed & Developed By Yuvocreations.com</p>
+                </div>
             </section>
         </>
     );
